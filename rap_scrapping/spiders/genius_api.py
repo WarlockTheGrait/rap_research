8d432c1d1ca9a961a937449f2ae501601f6f6402
RuslanOm <38555638+RuslanOm@users.noreply.github.com>
2018-07-22 16:35:40 +0300

    Create genius_api.py

---
+++ rap_scrapping/spiders/genius_api.py
@@ -0,0 +1,32 @@
+import lyricsgenius as genius
+import json
+
+
+def getting_names():
+    f = open("result.json", "r")
+    inform = json.loads(
+        f.read())
+    f.close()
+    names = []
+    for i in inform:
+        names.append(i["name"])
+    search(names)
+
+
+def search(names):
+    api = genius.Genius('OjplsjHuJuBJiqa_KqAfUz12EaUXkd2ETQwTNf-9J82qcjYRLY7OCoIpJzDOmQtF')
+    for i in names:
+        artist = api.search_artist(i) #вот здесь вот трабл с остановкой
+        file = artist.save_lyrics(format='json')
+        function(file)
+
+
+def function(file):
+    print(file["songs"][0]["lyrics"])
+    f = open("text.txt", 'a')
+    f.write(file["songs"][0]["lyrics"])
+    f.close()
+
+
+if __name__ == '__main__':
+    getting_names()
